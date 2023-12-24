adb296482a7ea2a017445c73015f2800c474a33a
unit-404 <elviras9t@gmail.com>
2023-12-24 14:36:45 +0000

    broken

--- BDesign/linking.sh
+++
@@ -1,3 +0,0 @@
-sudo ln -rdsP ../BTyped2 ./deps/BTyped2
-sudo ln -rdsP ../BasePlate ./deps/BasePlate
-sudo ln -rdsP ../BQuery ./deps/BQuery
--- BDesign/src/fonts/readme.md
+++
@@ -1,1 +0,0 @@
-Variable font file outputs (.ttf, .woff2, etc.) are placed in this folder.
